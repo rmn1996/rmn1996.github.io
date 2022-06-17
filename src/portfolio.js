bf2b47a1120a35f1f34daefdb6756901ffe8f5ec
Raman Agnihotri <rmn1996@gmail.com>
2022-06-17 12:07:43 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -19,8 +19,8 @@ const seo = {
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
+  title: "raman Hathidara",
+  logo_name: "raman",
   nickname: "layman_brother",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
